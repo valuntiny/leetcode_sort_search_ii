fbc095ea8e608c835d3ef6293521ae78f928a528
valuntiny <459201296@qq.com>
2019-02-24 20:35:57 -0500

    this method is what I called "peeling method", like peeling an apple

---
+++ searchMatrix.py
@@ -0,0 +1,54 @@
+'''
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+'''
+
+
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+
+        r, c = 0, len(matrix[0]) - 1
+        m, n = len(matrix) - 1, len(matrix[0]) - 1
+        while c >= 0 and r <= m:
+            if matrix[r][c] > target:
+                c -= 1
+            elif matrix[r][c] < target:
+                r += 1
+            else:
+                return True
+
+        return False
+
+test = Solution()
+matrix = [
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+target = 20
+print(test.searchMatrix(matrix, target))
